840d7d7e68350e10c639545852bd3e7dc0c81bf8
Donettello <alvaroalvarranz@gmail.com>
2026-02-25 18:52:59 +0100

    Actuaalizacion código

--- codigo/ibex_check.py
+++ codigo/ibex_check.py
@@ -2,6 +2,8 @@ import datetime
 import os
 import yfinance as yf
 
+from manejador_datos import manejador_csv
+
 def guardar_registro():
     ahora = datetime.datetime.now()
     mensaje = f"Se ha ejecutado el código el: {ahora.strftime('%d-%m-%Y %H:%M:%S')}"
@@ -41,10 +43,46 @@ def capturar_cierre():
             fecha_ultima = "" # Si el archivo no existe, lo tratamos como vacío
 
         if fecha_sesion == fecha_sistema and fecha_sesion != fecha_ultima:
-            print("Hoy toca trabajar")
-            # Hacer cosas próximamente
+            print(f"Obteniendo datos de la bolsa")
+
+            # Capturamos el precio de Inditex
+            ticket = "ITX.MC"
+            accion = yf.Ticker(ticket)
+            hist = accion.history(period="2d")
+
+            # Extraemos los precios
+            precio_apertura = hist['Open'].iloc[-1]
+            precio_cierre_hoy = hist['Close'].iloc[-1]
+            precio_cierre_ayer = hist['Close'].iloc[-2]
+
+            rentabilidad_dia = ((precio_cierre_hoy-precio_cierre_ayer)/precio_cierre_ayer) * 100
+            rentabilidad_sesion = ((precio_cierre_hoy-precio_apertura)/precio_apertura) * 100
+
+            # Preparamos el dato
+            nuevo_dato = {
+                'Fecha': fecha_sesion,
+                'Precio inicio sesion': round(precio_apertura, 4),
+                'Precio final sesion': round(precio_cierre_hoy, 4),
+                'Rentabilidad sesion': round(rentabilidad_sesion, 4),
+                'Rentabilidad diaria': round(rentabilidad_dia,4)
+            }
+
+            # Ruta de guardado de los datos
+            ruta_csv = "/home/donettello/Documents/VisualIBEX/data/datos_inditex.csv"
+            manejador_csv(nuevo_dato, ruta_csv, max_registros=20)
+            
+            # Actualizar fichero de control
             with open("/home/donettello/Documents/VisualIBEX/ultima_sesion.txt", 'w') as f:
                 f.write(fecha_sistema)
+            
+            #  Control de crontab
+            ahora = datetime.datetime.now()
+            mensaje = f"Se ha ejecutado el código el: {ahora.strftime('%d-%m-%Y %H:%M:%S')}"
+
+            with open("/home/donettello/Documents/VisualIBEX/ibex_log.txt", 'a') as f:
+                f.write(mensaje + "\n")
+            
+            print(f"Ficheros actuaalizados")
         else:
             print("Hoy no toca")
     
---
+++ codigo/manejador_datos.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+import datetime
+
+def manejador_csv(nueva_fila, ruta_csv, max_registros=20):
+    """
+        Guarda una fila en el CSV y mantiene los últimos X registros
+        nueva_fila: dict con formato {'Fecha':..., 
+                                      'Precio inicio sesion':..., 
+                                      'Precio final sesion':..., 
+                                      'Rentabilidad sesion':...,
+                                      'Rentabilidad diaria':...}
+    """
+    # Comprobamos que la dirección del fichero es la correcta
+    if os.path.exists(ruta_csv):
+        df = pd.read_csv(ruta_csv, sep=';')
+    else:
+        # En caso de que  no exista
+        df = pd.DataFrame(columns=['Fecha', 
+                                   'Precio inicio sesion', 
+                                   'Precio final sesion', 
+                                   'Rentabilidad sesion', 
+                                   'Rentabilidad diaria'])
+
+    # Añadimos la nueva fila al DataFrame
+    # Convertimos la nueva fila en un pequeño DataFrame
+    df_nueva = pd.DataFrame([nueva_fila])
+    df = pd.concat([df, df_nueva], ignore_index=True)
+
+    # Mantenemos sólo los últimos registros
+    if len(df) > max_registros:
+        df = df.tail(max_registros)
+
+    # Guardamos los datos en el CSV
+    df.to_csv(ruta_csv, index=False, sep=';')
+    print(f"Registro guardado. Total en archivo: {len(df)} lineas.")
+    return
+
